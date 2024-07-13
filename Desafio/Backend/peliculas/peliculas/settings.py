037ef7cb141bc7a2a6a8c89279868dc141749a83
sllachod <sllachod@unsa.edu.pe>
2024-07-12 23:09:51 -0500

    asignando la app instalada y comunicacion con frontend

--- Desafio/Backend/peliculas/peliculas/settings.py
+++ Desafio/Backend/peliculas/peliculas/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cineteca',
+    'rest_framework',  # Para usar Django REST framework
+    'corsheaders',  # Para permitir peticiones desde el frontend
 ]
 
 MIDDLEWARE = [
